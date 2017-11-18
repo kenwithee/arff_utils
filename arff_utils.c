e40dbef346de099d5d62effa6f528df5241c3428
Ken Withee <krw@scrumnow.com>
2017-11-17 20:57:11 -0800

    basic input

--- arff_utils.c
+++ arff_utils.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
+#define MAXLINE 5000
+
 int main()
 {
-   printf("Hello World\n");
+   char line[MAXLINE];
+   FILE *fp;
+   int c;
+   int i;
+
+   i = 0;
+
+   if ((fp = fopen("data/hello", "r")) == NULL)
+   {
+      printf("error opening file!\n");
+      return 1;
+   }
+   else
+   {
+      while (( c = getc(fp)) != EOF && i < MAXLINE)
+      {
+         line[i] = c;
+         i++;
+      }
+      printf("here is a line...\n%s", line);
+      printf("\n\n");
+      fclose(fp);
+   }
+
    return 0;
 }
